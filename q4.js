7c376e0275c2a85118cb776a1af93472778309c9
akrit-2007 <akritsingh69@gmail.com>
2026-04-08 21:36:33 +0530

    q4.js

--- q4.js
+++ q4.js
@@ -1,21 +1,22 @@
 // Question 4 - Fare Calculator
-
-let base = Number(prompt("Enter base:"));
-let distance = Number(prompt("Enter distance:"));
-let minutesLate = Number(prompt("Enter minutesLate:"));
-let seed = Number(prompt("Enter seed:"));
-
-let fare = base + 7 * distance;
-
+ 
+var base = Number(prompt("Enter base:"));
+var distance = Number(prompt("Enter distance:"));
+var minutesLate = Number(prompt("Enter minutesLate:"));
+var seed = Number(prompt("Enter seed:"));
+ 
+var fare = base + 7 * distance;
+ 
 if (minutesLate > 15) fare += 20;
 if (distance > 10) fare += Math.floor(0.10 * fare);
-
+ 
 if (seed % 2 !== 0) {
   fare -= seed;
 } else {
   fare += seed;
 }
-
+ 
 fare = Math.ceil(fare / 5) * 5;
-
+ 
 alert(fare);
+ 
