78f841133ab109ff2e5f507ec2e1c0482a5180ed
akrit-2007 <akritsingh69@gmail.com>
2026-04-08 21:33:31 +0530

    q1.js

--- q1.js
+++ q1.js
@@ -1,27 +1,28 @@
 // Question 1 - Digit Gatekeeper
-
+ 
 function isPrime(n) {
   if (n < 2) return false;
-  for (let i = 2; i <= Math.sqrt(n); i++) {
+  for (var i = 2; i <= Math.sqrt(n); i++) {
     if (n % i === 0) return false;
   }
   return true;
 }
-
-let L = Number(prompt("Enter L:"));
-let R = Number(prompt("Enter R:"));
-let K = Number(prompt("Enter K:"));
-
-let count = 0;
-
-for (let x = L; x <= R; x++) {
+ 
+var L = Number(prompt("Enter L:"));
+var R = Number(prompt("Enter R:"));
+var K = Number(prompt("Enter K:"));
+ 
+var count = 0;
+ 
+for (var x = L; x <= R; x++) {
   if (x % K !== 0) continue;
-
-  let digits = String(x).split("");
+ 
+  var digits = String(x).split("");
   if (digits.includes("0")) continue;
-
-  let digitSum = digits.reduce((sum, d) => sum + Number(d), 0);
+ 
+  var digitSum = digits.reduce((sum, d) => sum + Number(d), 0);
   if (isPrime(digitSum)) count++;
 }
-
+ 
 alert(count);
+ 
