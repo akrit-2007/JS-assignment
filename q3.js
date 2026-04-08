3454cce044e1580e620eb345f3d772a6065799ac
akrit-2007 <akritsingh69@gmail.com>
2026-04-08 21:35:50 +0530

    q3.js

--- q3.js
+++ q3.js
@@ -1,22 +1,23 @@
 // Question 3 - Mirror Corridor
-
+ 
 function isPalindrome(n) {
-  let s = String(n);
+  var s = String(n);
   return s === s.split("").reverse().join("");
 }
-
-let N = Number(prompt("Enter N:"));
-let K = Number(prompt("Enter K:"));
-
-let found = false;
-
-for (let X = 0; X <= 100000; X++) {
-  let val = N + X;
+ 
+var N = Number(prompt("Enter N:"));
+var K = Number(prompt("Enter K:"));
+ 
+var found = false;
+ 
+for (var X = 0; X <= 100000; X++) {
+  var val = N + X;
   if (isPalindrome(val) && val % K === 0) {
     alert(X);
     found = true;
     break;
   }
 }
-
+ 
 if (!found) alert(-1);
+ 
