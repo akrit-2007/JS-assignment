fb53e41a1ae7c0f87f1da4860e47849c1273ec03
akrit-2007 <akritsingh69@gmail.com>
2026-04-08 21:34:31 +0530

    q2.js

--- q2.js
+++ q2.js
@@ -1,22 +1,23 @@
 // Question 2 - Roll-Seed Lock
-
-let N = Number(prompt("Enter N:"));
-let seed = Number(prompt("Enter seed:"));
-
-let current = N;
-
-for (let i = 0; i < 3; i++) {
+ 
+var N = Number(prompt("Enter N:"));
+var seed = Number(prompt("Enter seed:"));
+ 
+var current = N;
+ 
+for (var i = 0; i < 3; i++) {
   if (current % 2 === 0) {
     current = current / 2 + seed;
   } else {
     current = current * 3 - seed;
   }
 }
-
-let s = String(current);
-
+ 
+var s = String(current);
+ 
 if (current >= 100 && current <= 999 && Number(s[1]) === seed) {
   alert("YES, " + current);
 } else {
   alert("NO, " + current);
 }
+ 
