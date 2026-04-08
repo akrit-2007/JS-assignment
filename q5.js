52a3362712dc518c9ba03148dcdb133d0b25eeb6
akrit-2007 <akritsingh69@gmail.com>
2026-04-08 21:37:21 +0530

    q5.js

--- q5.js
+++ q5.js
@@ -1,17 +1,18 @@
 // Question 5 - Skipping Numbers
-
-let N = Number(prompt("Enter N:"));
-let seed = Number(prompt("Enter seed:"));
-
-let divisor = seed + 2;
-let sum = 0;
-let m = 0;
-
+ 
+var N = Number(prompt("Enter N:"));
+var seed = Number(prompt("Enter seed:"));
+ 
+var divisor = seed + 2;
+var sum = 0;
+var m = 0;
+ 
 while (sum < N) {
   m++;
   if (m % divisor !== 0) {
     sum += m;
   }
 }
-
+ 
 alert("m = " + m + ", sum = " + sum);
+ 
